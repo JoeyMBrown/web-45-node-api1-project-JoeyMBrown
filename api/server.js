d9078420468868c1058ad16dd87ad3dec6dd5c58
Joey_M_Brown <joey.ma.brown@gmail.com>
2021-08-31 01:02:06 -0400

    Exported server.  Started server instance.  Added express to dependencies

--- api/server.js
+++ api/server.js
@@ -1,3 +1,16 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json()) //Teaches express to read json
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'server working'})
+})
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: 'sorry, no route found!'}) //Order matters!
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
